cad758ae460f92c29e2b5bd8f3457cdaf2e21723
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-06-13 15:02:43 -0700

    Current Display Changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -40,6 +40,44 @@ var getCityLatLong = function (city) {
       });
   };
 
+  var createCurrentDayWeather = function (city , currentDayData) {
+    // Create Card Div for current day weather content
+    var cardEl = document.createElement('div')
+    cardEl.classList = 'card'
+    // Create Card Header 
+    var cardHeaderEl = document.createElement('h3')
+    cardHeaderEl.classList = 'card-header'
+    cardHeaderEl.textContent = city + ' ' + dayjs.unix(currentDayData.dt).format('MM/DD/YYYY')
+
+    var iconEl = document.createElement('img')
+
+    iconEl.setAttribute('alt', 'Weather icon')
+    iconEl.setAttribute('src', 'http://openweathermap.org/img/w/' + currentDayData.weather[0].icon + '.png')
+
+    cardHeaderEl.appendChild(iconEl)
+    cardEl.appendChild(cardHeaderEl)
+
+    // Create Card Body Div 
+    var cardBodyEl = document.createElement('div')
+    cardBodyEl.classList = 'card-body'
+
+    var cardDetails = document.createElement('p')
+    
+    // Convert from Kelvin to Fahrenheit
+    //let minTemp = ((currentDayData.main.temp_min - 273.15) * 9/5) + 32
+    //let maxTemp = ((currentDayData.main.temp_max - 273.15) * 9/5) + 32
+    let temp = Math.round(((currentDayData.main.temp - 273.15) * 9/5) + 32)
+    
+    // Covert meter/sec to Miles per hour
+    let speed = Math.round(currentDayData.wind.speed * 2.237 * 2)/2
+
+    cardDetails.innerHTML = "Temp " + temp + "&deg;F <br> Wind " + Math.round(currentDayData.wind.speed * 2.237 * 2)/2 + " MPH <br> Humidity " + currentDayData.main.humidity + " %"
+    cardBodyEl.appendChild(cardDetails)
+    
+    cardEl.appendChild(cardBodyEl)
+    weatherContainerEl.appendChild(cardEl)
+  }
+
   var getWeatherContent = function (lat,lng) {
     var apiForecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lng + '&appid=' + apiKey;
   
@@ -48,9 +86,39 @@ var getCityLatLong = function (city) {
         if (response.ok) {
           console.log(response);
           response.json().then(function (data) {
-            console.log(data);
 
-            weatherContainerEl.textContent = JSON.stringify(data)
+            console.log(data);
+            // Display current day weather
+            createCurrentDayWeather(data.city.name, data.list[0])
+
+            
+
+            /*for (var i = 0; i < data.length; i++) {
+                var repoName = repos[i].owner.login + '/' + repos[i].name;
+
+                var repoEl = document.createElement('a');
+                repoEl.classList = 'list-item flex-row justify-space-between align-center';
+                repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
+
+                var titleEl = document.createElement('span');
+                titleEl.textContent = repoName;
+
+              repoEl.appendChild(titleEl);
+
+              var statusEl = document.createElement('span');
+              statusEl.classList = 'flex-row align-center';
+
+              if (repos[i].open_issues_count > 0) {
+                statusEl.innerHTML =
+                  "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+              } else {
+                statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+              }
+
+              repoEl.appendChild(statusEl);
+
+              repoContainerEl.appendChild(repoEl);
+            } */
             
           });
         } else {
@@ -64,6 +132,7 @@ var getCityLatLong = function (city) {
 
 
   var processCityLatLong = function(data, city) {
+
     if (data.length === 0) {
       weatherContainerEl.textContent = 'No cities found for search criteria.';
       return;
@@ -73,49 +142,14 @@ var getCityLatLong = function (city) {
     lng = data[0].lon;
 
     var buttonEl = document.querySelector('#js1');
-    //buttonEl.click();
     buttonEl.ariaPressed = "true";
 
     buttonClickHandler(city)
   }
 
   var buttonClickHandler = function(city) {
-
-    weatherContainerEl.textContent =   city;
-
+    weatherContainerEl.innerHTML = '';
     getWeatherContent(lat,lng)
-    
-  /*for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-    var repoEl = document.createElement('a');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-    repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }*/
-
   }
-
-  
-
-
 
   userFormEl.addEventListener('submit', formSubmitHandler);
